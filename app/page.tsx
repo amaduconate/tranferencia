6b701cc7018975ae8a4bc3960f7900fb9ba4d603
amaduconate <amaduconate2gmail.com>
2024-11-25 01:19:47 +0300

    fix it

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import InterTransfer from './ui/intertransfer'
-import Exemple from './ui/exemple'
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-r from-green-100 to-blue-100'>
 			<InterTransfer />
-			<Exemple/>
 		</main>
 	)
 }
