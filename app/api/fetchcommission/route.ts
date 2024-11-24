cd23f13cd4f745a98d4a2ff968791fcdfe3e2aec
amaduconate <amaduconate2gmail.com>
2024-11-25 01:44:50 +0300

    fix it again

--- app/api/fetchcommission/route.ts
+++ app/api/fetchcommission/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server'
 import { PrismaClient } from '@prisma/client'
+export const dynamic = 'force-dynamic'
 // this is api to fetch commission
 const updateCurrency = async () => {
   
--- app/api/updaterate/route.ts
+++ app/api/updaterate/route.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server'
+export const dynamic = 'force-dynamic'
 
 const APIKEY = process.env.APIKEY as 'KEY'
 
