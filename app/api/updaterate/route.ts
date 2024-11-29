5f43845b3688b2d8649c4e471adbe75208fd302e
amaduconate <81099798+amaduconate@users.noreply.github.com>
2024-11-29 06:29:28 +0000

    update rated

--- app/api/updaterate/route.ts
+++ app/api/updaterate/route.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
+export const revalidate = 1;
 
 const APIKEY = process.env.APIKEY as "KEY";
 
@@ -56,19 +57,58 @@ const updateRate = async (
   });
 };
 
-const startcronjob = async () => {
-  await fetchRate().then((rates) => {
-    updateRate(rates);
-  });
+// const startcronjob = async () => {
+//   await fetchRate().then((rates) => {
+//     updateRate(rates);
+//   });
 
-  // setInterval(async () => {
-  // 	const rate = await fetchRate()
-  // 	await updateRate(rate)
-  // }, 6 * 60 * 60 * 1000)
-};
+// setInterval(async () => {
+// 	const rate = await fetchRate()
+// 	await updateRate(rate)
+//   // }, 6 * 60 * 60 * 1000)
+// };
 
 export async function GET() {
-  await startcronjob();
+  // await startcronjob();
+
+  const rateEur = await fetch(
+    `https://v6.exchangerate-api.com/v6/${APIKEY}/pair/EUR/RUB`
+  );
+  const rateRub = await fetch(
+    `https://v6.exchangerate-api.com/v6/${APIKEY}/pair/RUB/XOF`
+  );
+  const rateXof = await fetch(
+    `https://v6.exchangerate-api.com/v6/${APIKEY}/pair/XOF/RUB`
+  );
+
+  const dataEur = await rateEur.json();
+  const dataRub = await rateRub.json();
+  const dataXof = await rateXof.json();
+
+  const euroRate: number = (await dataEur.conversion_rate) as number;
+  const rubleRate: number = (await dataRub.conversion_rate) as number;
+  const xofRate: number = (await dataXof.conversion_rate) as number;
+
+  const datareturn = {
+    fromEur: euroRate,
+    fromRub: rubleRate,
+    fromXof: xofRate,
+  };
+  if (!datareturn) {
+    return;
+  }
+  console.log(datareturn);
+  const prisma = new PrismaClient();
+  await prisma.exchange_rate.update({
+    where: {
+      id: 1,
+    },
+    data: {
+      fromEur: datareturn.fromEur,
+      fromRub: datareturn.fromRub,
+      fromXof: datareturn.fromXof,
+    },
+  });
   return NextResponse.json({
     message: "Exchange rate cron job updated successfully",
   });
