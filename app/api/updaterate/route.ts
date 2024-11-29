a6f9277e309b45b73f4cd87a16b9569f85f406eb
amaduconate <81099798+amaduconate@users.noreply.github.com>
2024-11-29 06:36:49 +0000

    update

--- app/api/updaterate/route.ts
+++ app/api/updaterate/route.ts
@@ -5,57 +5,57 @@ export const revalidate = 1;
 
 const APIKEY = process.env.APIKEY as "KEY";
 
-const fetchRate = async () => {
-  try {
-    const rateEur = await fetch(
-      `https://v6.exchangerate-api.com/v6/${APIKEY}/pair/EUR/RUB`
-    );
-    const rateRub = await fetch(
-      `https://v6.exchangerate-api.com/v6/${APIKEY}/pair/RUB/XOF`
-    );
-    const rateXof = await fetch(
-      `https://v6.exchangerate-api.com/v6/${APIKEY}/pair/XOF/RUB`
-    );
+// const fetchRate = async () => {
+//   try {
+//     const rateEur = await fetch(
+//       `https://v6.exchangerate-api.com/v6/${APIKEY}/pair/EUR/RUB`
+//     );
+//     const rateRub = await fetch(
+//       `https://v6.exchangerate-api.com/v6/${APIKEY}/pair/RUB/XOF`
+//     );
+//     const rateXof = await fetch(
+//       `https://v6.exchangerate-api.com/v6/${APIKEY}/pair/XOF/RUB`
+//     );
 
-    const dataEur = await rateEur.json();
-    const dataRub = await rateRub.json();
-    const dataXof = await rateXof.json();
+//     const dataEur = await rateEur.json();
+//     const dataRub = await rateRub.json();
+//     const dataXof = await rateXof.json();
 
-    const euroRate: number = (await dataEur.conversion_rate) as number;
-    const rubleRate: number = (await dataRub.conversion_rate) as number;
-    const xofRate: number = (await dataXof.conversion_rate) as number;
+//     const euroRate: number = (await dataEur.conversion_rate) as number;
+//     const rubleRate: number = (await dataRub.conversion_rate) as number;
+//     const xofRate: number = (await dataXof.conversion_rate) as number;
 
-    return {
-      fromEur: euroRate,
-      fromRub: rubleRate,
-      fromXof: xofRate,
-    };
-  } catch (error) {
-    console.log(error);
-    console.log(
-      "something has gone wrong in fetching the data in updateRate api"
-    );
-    return null;
-  }
-};
+//     return {
+//       fromEur: euroRate,
+//       fromRub: rubleRate,
+//       fromXof: xofRate,
+//     };
+//   } catch (error) {
+//     console.log(error);
+//     console.log(
+//       "something has gone wrong in fetching the data in updateRate api"
+//     );
+//     return null;
+//   }
+// };
 
-const updateRate = async (
-  rates: { fromEur: number; fromRub: number; fromXof: number } | null
-) => {
-  if (!rates) return;
-  console.log(rates.fromEur);
-  const prisma = new PrismaClient();
-  await prisma.exchange_rate.update({
-    where: {
-      id: 1,
-    },
-    data: {
-      fromEur: rates.fromEur,
-      fromRub: rates.fromRub,
-      fromXof: rates.fromXof,
-    },
-  });
-};
+// const updateRate = async (
+//   rates: { fromEur: number; fromRub: number; fromXof: number } | null
+// ) => {
+//   if (!rates) return;
+//   console.log(rates.fromEur);
+//   const prisma = new PrismaClient();
+//   await prisma.exchange_rate.update({
+//     where: {
+//       id: 1,
+//     },
+//     data: {
+//       fromEur: rates.fromEur,
+//       fromRub: rates.fromRub,
+//       fromXof: rates.fromXof,
+//     },
+//   });
+// };
 
 // const startcronjob = async () => {
 //   await fetchRate().then((rates) => {
